977c9f7d47fdf5a5fb3725cdc1554170bd5359df
Shalini Pathak <86199053+letslearnn@users.noreply.github.com>
2022-07-07 03:20:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,6 +42,18 @@ function createName() {
   btn.innerHTML = "Click Me";
   document.body.appendChild(btn);
   
+  var button = document.createElement("button");
+  button.innerHTML = "Do Something";
+
+  // 2. Append somewhere
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(button);
+
+  // 3. Add event handler
+  button.addEventListener ("click", function() {
+  alert("did something");
+});
+  
   document.querySelector(".naga").style.bottom = "20px";
   audio = document.createElement("audio");
   audio.src = "./atrangi.mp3";
