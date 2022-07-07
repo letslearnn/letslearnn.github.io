67060ec88f19f3af799745e3c8270e7c55640efc
Shalini Pathak <86199053+letslearnn@users.noreply.github.com>
2022-07-07 13:57:05 +0530

    Update texta.js

--- texta.js
+++ texta.js
@@ -1,6 +1,6 @@
 const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
 };
 
 const startButton = document.querySelector(".start-button");
@@ -21,69 +21,69 @@ elts.text1.textContent = texts[textIndex % texts.length];
 elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
 function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
+  morph -= cooldown;
+  cooldown = 0;
 
-    let fraction = morph / morphTime;
+  let fraction = morph / morphTime;
 
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
 
-    setMorph(fraction);
+  setMorph(fraction);
 }
 
 function setMorph(fraction) {
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 }
 
 function doCooldown() {
-    morph = 0;
+  morph = 0;
 
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
 
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
 
-    cooldown -= dt;
+  cooldown -= dt;
 
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
-
-        doMorph();
-    } else {
-        doCooldown();
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
     }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
 }
 
 //animate();
 startButton.onclick = () => {
-    startButton.classList.add("hidden");
+  startButton.classList.add("hidden");
 
-    lottie.play();
-    lottie.addEventListener("complete", () => {
-        container.classList.remove("hidden");
-        animate();
-    });
+  lottie.play();
+  lottie.addEventListener("complete", () => {
+    container.classList.remove("hidden");
+    animate();
+  });
 };
